70983ad3a4f24f33dbd2fbf431aaa92ff669a054
joy <j.selva@naviger.it>
2022-06-24 10:51:51 +0200

    added seeders

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import admin = require('firebase-admin');
 import { seed } from "./collection_seed";
+import { CreateMovement } from './seeders/create_movement';
 
 const movement_create_listener = require('./movement_create_listener');
 const movement_update_listener = require('./movement_update_listener');
@@ -8,6 +9,8 @@ admin.initializeApp();
 
 // seed dummy data for testing
 seed();
+
+CreateMovement.run(1000);
 
 exports.movement_create_listener = movement_create_listener.onListen;
 exports.movement_update_listener = movement_update_listener.onListen;
---
+++ functions/src/seeders/create_movement.ts
@@ -0,0 +1,28 @@
+import admin = require('firebase-admin');
+
+export class CreateMovement {
+    static async run(times: number = 100) {
+        const db = admin.firestore();
+        var batch = db.batch();
+        for (var i = 0; i < times; i++) {
+            var docRef = db.collection("movements").doc();
+            var type:number =  this.getRandomInt(2);
+            batch.set(docRef, {
+                amount: type == 1 ? 10  : this.getRandomInt(2),
+                debit: type == 1 ? false  : true,
+                line: this.getRandomInt(14),
+                machine_code: "2323",
+                moviment_at: Date(),
+                moviment_type: type,
+                portfolio_code: "001",
+                user_ref: "abcd"
+            });
+            console.info(">>>> seeding movement" + i);
+        }
+        batch.commit();
+    }
+
+    static getRandomInt(max:number) : number {
+        return Math.floor(Math.random() * max);
+    }
+}
